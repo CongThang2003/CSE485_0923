49b750c63596c1bc40582231c956c39f6848f9ce
CongThang2003 <128068649+CongThang2003@users.noreply.github.com>
2023-09-23 02:35:55 +0700

    Delete BTTH01B/Admin directory

--- BTTH01B/Admin/layout_footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Phần footer -->
-<hr class="height: 5vh;">
-<div class="footer">
-    <div class="w-100" style="text-align: center;">
-        <span style="font-weight: 600;">TLU'S MUSIC GARDEN</span>
-    </div>
-</div>
